fffca5e3af295a6b8c80d82f68b4777c1b72ce84
sumit <sumitthapa90@gmail.com>
2021-08-29 15:04:57 +0530

    complete

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,16 +1,13 @@
-const fs = require("fs");
+var { readFile, readFileSync } = require("fs");
 
-fs.readFile("./content.md", (err, file) => {
-  console.log(file.toString());
+readFile("./content.md", "utf8", (err, content) => {
+  console.log(content);
 });
-
-let runSyn = fs.readFile("./content.md", "utf-8");
-
-console.log(runSyn);
+var result = readFileSync("./content.md", "utf8");
+console.log(result);
 
 let buff1 = Buffer.alloc(10);
 
-console.log(buff1);
+buff1;
 
-buff1.write("Welcome to buffer");
-console.log(buff1.toString());
+buff1.write("Welcome to Buffer")
