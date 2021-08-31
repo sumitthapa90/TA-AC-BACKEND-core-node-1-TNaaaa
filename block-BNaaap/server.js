03af975fd64fed415c58bae7000ba0213295e023
sumit <sumitthapa90@gmail.com>
2021-08-31 13:10:24 +0530

    complete

---
+++ block-BNaaap/server.js
@@ -0,0 +1,16 @@
+// 1. create a server on port 4444 and request from browser on `localhost:4444`
+// 2. Add statusCode of 201 and headers to send html content to response
+// 3. Do request on server using **different HTTP methods** using **Postman** and write code to check request methods for multiple requests.
+
+var http = require("http");
+
+var server = http.createServer(handleServer);
+
+function handleServer(res, req) {
+  res.statusCode = 201;
+  res.setHeader("./Content-Type", "text/html");
+  res.end("<h1>Welcom Alt Campus</h1>");
+}
+server.listen(4444, () => {
+  console.log("Server listing on port 4444");
+});
