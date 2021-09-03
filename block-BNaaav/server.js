72ff83744c7c2a2cf10075814f5ec8b654e3542f
sumit <sumitthapa90@gmail.com>
2021-09-03 10:32:02 +0530

    BNaaav again

--- block-BNaaav/server.js
+++ block-BNaaav/server.js
@@ -4,27 +4,24 @@ var fs = require("fs");
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-  if (req.url === "./") {
+  if (req.method === "GET" && req.url === "/") {
+    console.log(req.method, req.url);
     res.setHeader("Content-Type", "text/html");
     fs.createReadStream("./index.html").pipe(res);
-  } else if (req.url === "./about") {
+  } else if (req.method === "GET" && req.url === "./about") {
     res.setHeader("Content-Type", "text/html");
     fs.createReadStream("./about.html").pipe(res);
   } else if (
-    req.url.split(".").pop() === "jpg" ||
-    req.url.split(".").pop() === "png"
+    req.url.spilt(".").pop() === "jpg" ||
+    req.url.spilt(".").pop() === "png"
   ) {
-    res.setHeader("Content-Type", "image/jpg");
+    req.setHeader("Content-Type", "image/css");
     fs.createReadStream(`./${req.url}`).pipe(res);
-  } else if (req.url.split(".").pop() === "jpg") {
-    res.setHeader("Content-Type", "text/js");
-    fs.createReadStream(`./${req.url}`).pipe(res);
-  } else {
-    res.statusCode = 404;
-    res.ens("Page not found");
-  }
+  } else if (req.url.spilt(".").pop() === "app.js") {
+    req.setHeader("Content-Type", "text/app.js");
+  } else res.end("Error");
 }
 
 server.listen(5500, () => {
-  console.log("server listi at port 5500");
+  console.log("server lising at port 5500");
 });
